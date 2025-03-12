6118672553b009dd4f1001b2466e920b6e79634e
Pranjal Chaurasia <pranjalchaurasia11@gmail.com>
2025-03-13 03:42:13 +0530

    register user

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -31,3 +31,19 @@ const userSchema = new mongoose.Schema({
         required:false,
     },
 })
+
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id:this._id},process.env.JWT_SECRET);
+    return token;
+}
+
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+
+userSchema.methods.hashPassword = async function(password){
+    this.password = await bcrypt.hash(this.password,10);
+}
+
+const userModel = mongoose.model('User',userSchema); 
+module.exports = userModel;
