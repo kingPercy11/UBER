c83edd22e2b72d877277c353bd8c1fe07009c8d3
Pranjal Chaurasia <pranjalchaurasia11@gmail.com>
2025-03-16 21:25:46 +0530

    driver signup page

--- Frontend/src/pages/DriverSignup.jsx
+++ Frontend/src/pages/DriverSignup.jsx
@@ -1,10 +1,98 @@
-import React from "react"
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
 
-const DriverSignup = () => {    
+const DriverSignup = () => { 
+      const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+  const [ firstName, setFirstName ] = useState('')
+  const [ lastName, setLastName ] = useState('')
+  const [ driverData, setDriverData ] = useState({})
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    const newDriver = {
+      fullname: {
+        firstname: firstName,
+        lastname: lastName
+      },
+      email: email,
+      password: password
+    }
+    // console.log(newDriver)
+    setEmail('')
+    setFirstName('')
+    setLastName('')
+    setPassword('')
+
+  }    
     return (
-        <div>
-            <h1>DriverSignup</h1>
-            <p>Welcome to the DriverSignup page!</p>
+        <div> 
+            <div className='px-5 py-5 h-screen flex flex-col justify-between'>
+                <div>
+                    <img className='w-16 mb-10' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYQy-OIkA6In0fTvVwZADPmFFibjmszu2A0g&s" alt="" />
+                    <form onSubmit={(e) => {
+                        submitHandler(e)
+                    }}>
+
+                        <h3 className='text-lg w-1/2  font-medium mb-2'>What's your name</h3>
+                        <div className='flex gap-4 mb-7'>
+                        <input
+                            required
+                            className='bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border  text-lg placeholder:text-base'
+                            type="text"
+                            placeholder='First name'
+                            value={firstName}
+                            onChange={(e) => {
+                            setFirstName(e.target.value)
+                            }}
+                        />
+                        <input
+                            required
+                            className='bg-[#eeeeee] w-1/2  rounded-lg px-4 py-2 border  text-lg placeholder:text-base'
+                            type="text"
+                            placeholder='Last name'
+                            value={lastName}
+                            onChange={(e) => {
+                            setLastName(e.target.value)
+                            }}
+                        />
+                        </div>
+
+                        <h3 className='text-lg font-medium mb-2'>What's your email</h3>
+                        <input
+                        required
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value)
+                        }}
+                        className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
+                        type="email"
+                        placeholder='email@example.com'
+                        />
+
+                        <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
+
+                        <input
+                        className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value)
+                        }}
+                        required type="password"
+                        placeholder='password'
+                        />
+
+                        <button
+                        className='bg-[#111] text-white font-semibold mb-3 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base'
+                        >Create account</button>
+
+                    </form>
+                    <p className='text-center'>Already associated with us? <Link to='/driver-login' className='text-blue-600'>Login here</Link></p>
+                </div>
+                <div>
+                    <p className='text-[10px] leading-tight'>This site is protected by reCAPTCHA and the <span className='underline'>Google Privacy
+                        Policy</span> and <span className='underline'>Terms of Service apply</span>.</p>
+                </div>
+            </div>
         </div>
     )
 }
--- Frontend/src/pages/UserSignup.jsx
+++ Frontend/src/pages/UserSignup.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link} from 'react-router-dom'
 
 
 const UserSignup = () => {
@@ -19,7 +19,7 @@ const UserSignup = () => {
       password: password
 
     }
-    console.log(newUser)
+    // console.log(newUser)
     setEmail('')
     setFirstName('')
     setLastName('')
