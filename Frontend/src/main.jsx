11f5fb5e563e8f95a8afa6ddfdc35727cfa090ec
Pranjal Chaurasia <pranjalchaurasia11@gmail.com>
2025-03-17 13:39:57 +0530

    driver context, driver signup

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,13 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import UserContext from './context/UserContext'
+import DriverContext from './context/DriverContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <DriverContext>
     <UserContext>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </UserContext>
+    </DriverContext>
   </StrictMode>,
 )
--- Frontend/src/pages/DriverSignup.jsx
+++ Frontend/src/pages/DriverSignup.jsx
@@ -1,27 +1,55 @@
 import React, { useState } from "react"
 import { Link } from "react-router-dom"
+import { DriverDataContext } from "../context/DriverContext"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
 
 const DriverSignup = () => { 
-      const [ email, setEmail ] = useState('')
+  const [ email, setEmail ] = useState('')
   const [ password, setPassword ] = useState('')
   const [ firstName, setFirstName ] = useState('')
   const [ lastName, setLastName ] = useState('')
-  const [ driverData, setDriverData ] = useState({})
+  const [ vehicleColor, setVehicleColor ] = useState('')
+  const [ vehiclePlate, setVehiclePlate ] = useState('')
+  const [ vehicleCapacity, setVehicleCapacity ] = useState('')
+  const [ vehicleType, setVehicleType ] = useState('')
+
+  const { driver, setDriver } = React.useContext(DriverDataContext)   
+  const navigate = useNavigate()
+
+
   const submitHandler = async (e) => {
     e.preventDefault()
-    const newDriver = {
-      fullname: {
-        firstname: firstName,
-        lastname: lastName
-      },
-      email: email,
-      password: password
+    const driverData = {
+        fullname: {
+            firstname: firstName,
+            lastname: lastName
+        },
+        email: email,
+        password: password,
+        vehicle: {
+            color: vehicleColor,
+            plate: vehiclePlate,
+            capacity: vehicleCapacity,
+            vehicleType: vehicleType
+        }
+    }
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/drivers/register`, driverData)
+    if (response.status === 201) {
+      const data = response.data
+      setDriver(data.driver)
+      localStorage.setItem('token', data.token)
+      navigate('/home')
     }
     // console.log(newDriver)
     setEmail('')
     setFirstName('')
     setLastName('')
     setPassword('')
+    setVehicleColor('')
+    setVehiclePlate('')
+    setVehicleCapacity('')
+    setVehicleType('')
 
   }    
     return (
@@ -80,7 +108,54 @@ const DriverSignup = () => {
                         required type="password"
                         placeholder='password'
                         />
-
+                        <h3 className='text-lg font-medium mb-2'>Vehicle Information</h3>
+                        <div className='flex gap-4 mb-7'>
+                            <input
+                            required
+                            className='bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base'
+                            type="text"
+                            placeholder='Vehicle Color'
+                            value={vehicleColor}
+                            onChange={(e) => {
+                                setVehicleColor(e.target.value)
+                            }}
+                            />
+                            <input
+                            required
+                            className='bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base'
+                            type="text"
+                            placeholder='Vehicle Plate'
+                            value={vehiclePlate}
+                            onChange={(e) => {
+                                setVehiclePlate(e.target.value)
+                            }}
+                            />
+                        </div>
+                        <div className='flex gap-4 mb-7'>
+                            <input
+                            required
+                            className='bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base'
+                            type="number"
+                            placeholder='Vehicle Capacity'
+                            value={vehicleCapacity}
+                            onChange={(e) => {
+                                setVehicleCapacity(e.target.value)
+                            }}
+                            />
+                            <select
+                            required
+                            className='bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base'
+                            value={vehicleType}
+                            onChange={(e) => {
+                                setVehicleType(e.target.value)
+                            }}
+                            >
+                            <option value="" disabled>Select Vehicle Type</option>
+                            <option value="car">Car</option>
+                            <option value="auto">Auto</option>
+                            <option value="moto">Moto</option>
+                            </select>
+                        </div>
                         <button
                         className='bg-[#111] text-white font-semibold mb-3 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base'
                         >Create account</button>
