51f54c8f71bcf5377bf7511bd551902449c319a2
Pranjal Chaurasia <pranjalchaurasia11@gmail.com>
2025-03-21 04:54:29 +0530

    location search panel added

---
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,35 @@
+
+import React from "react";
+
+const LocationSearchPanel = () => { 
+    return (
+        <div> 
+            <div className="flex items-center justify-start">
+            <h2 className='bg-[#eee] h-8 flex items-center justify-center w-8 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+            <h4 className='font-medium'>Delhi Technological University, Delhi-110042</h4>
+            </div>
+            
+            <div className="flex items-center justify-start">
+            <h2 className='bg-[#eee] h-8 flex items-center justify-center w-8 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+            <h4 className='font-medium'>Delhi Technological University, Delhi-110042</h4>
+            </div>
+            
+            <div className="flex items-center justify-start">
+            <h2 className='bg-[#eee] h-8 flex items-center justify-center w-8 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+            <h4 className='font-medium'>Delhi Technological University, Delhi-110042</h4>
+            </div>
+            
+            <div className="flex items-center justify-start">
+            <h2 className='bg-[#eee] h-8 flex items-center justify-center w-8 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+            <h4 className='font-medium'>Delhi Technological University, Delhi-110042</h4>
+            </div>
+
+            <div className="flex items-center justify-start">
+            <h2 className='bg-[#eee] h-8 flex items-center justify-center w-8 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+            <h4 className='font-medium'>Delhi Technological University, Delhi-110042</h4>
+            </div>
+        </div>
+    )
+}
+
+export default LocationSearchPanel;
